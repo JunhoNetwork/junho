2893faba632206b6bbef38ce81265f4346f2362c
Junho Yeo <i@junho.io>
2023-05-15 20:56:43 +0900

    Fix tests

--- x/feeshare/keeper/keeper_test.go
+++ x/feeshare/keeper/keeper_test.go
@@ -42,7 +42,8 @@ type IntegrationTestSuite struct {
 
 func (s *IntegrationTestSuite) SetupTest() {
 	isCheckTx := false
-	s.app = app.Setup(s.T(), isCheckTx, 1)
+	// s.app = app.Setup(s.T(), isCheckTx, 1)
+	s.app = app.Setup(s.T())
 
 	s.ctx = s.app.BaseApp.NewContext(isCheckTx, tmproto.Header{
 		ChainID: fmt.Sprintf("test-chain-%s", tmrand.Str(4)),
--- x/mint/module_test.go
+++ x/mint/module_test.go
@@ -3,29 +3,17 @@ package mint_test
 import (
 	"testing"
 
+	"cosmossdk.io/simapp"
 	abcitypes "github.com/cometbft/cometbft/abci/types"
 	tmproto "github.com/cometbft/cometbft/proto/tendermint/types"
-	"github.com/cosmos/cosmos-sdk/testutil/configurator"
 	"github.com/stretchr/testify/require"
 
 	"github.com/JunhoNetwork/junho/x/mint/types"
-	simtestutil "github.com/cosmos/cosmos-sdk/testutil/sims"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 )
 
 func TestItCreatesModuleAccountOnInitBlock(t *testing.T) {
-	app, _ := simtestutil.Setup(
-		configurator.NewAppConfig(
-			configurator.ParamsModule(),
-			configurator.AuthModule(),
-			configurator.StakingModule(),
-			configurator.SlashingModule(),
-			configurator.TxModule(),
-			configurator.ConsensusModule(),
-			configurator.BankModule(),
-		),
-		false,
-	)
+	app := simapp.Setup(t, false)
 	ctx := app.BaseApp.NewContext(false, tmproto.Header{})
 
 	app.InitChain(
